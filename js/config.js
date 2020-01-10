f7d8de5a831a9381a9bd21cf010df9e4b659c0d0
RexBen117 <rexangeles.117@gmail.com>
2020-01-10 18:50:03 +0000

    update cognito

--- js/config.js
+++ js/config.js
@@ -2,7 +2,7 @@ window._config = {
     cognito: {
         userPoolId: 'ap-northeast-2_T4qRdGItE', // e.g. us-east-2_uXboG5pAb
         userPoolClientId: 'csq8t339qn4c2q66mdck0lsss', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: 'us-east-1' // e.g. us-east-2
+        region: 'ap-northeast-2' // e.g. us-east-2
     },
     api: {
         invokeUrl: 'https://v8z0gpwgga.execute-api.ap-northeast-2.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
